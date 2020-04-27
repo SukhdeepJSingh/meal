9a581fbec18dabb7e8d167935e4f130ad7e7436a
Sukhdeep Singh <privatesukhdeep@gmail.com>
2020-04-27 20:56:20 +0530

    Add responsive layout

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # meal
-A vanilla javascript app to search &amp; cook a meal.
+
+An app made with Vanilla JavaScript.
+
+How to:
+Enter name of any meal, chicken, for instance, and hit enter.
+Hit confused button to get a random meal.
+
+Features:
+
+1. Search bar
+2. Responsive
+
+Powered by 'themealdb.com' API.
--- script.js
+++ script.js
@@ -12,37 +12,47 @@ const form = $id('form');
 const searchTerm = $id('search-term');
 const meals = $id('meals');
 const mealDetail = $id('meal-detail');
+const randomBtn = $id('random-btn');
 
 // Get meals
 function searchAndRenderMeals(event) {
     event.preventDefault();
-    meals.innerHTML = '';
+    meals.innerHTML = '<div class="u-meals">Searching...</div>';
 
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm.value}`)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
-            meals.innerHTML = data.meals
-                .map(
-                    (meal) => `
+            if (!data.meals) {
+                meals.innerHTML = '<div class="u-meals">No results. Try again.</div>';
+            } else {
+                meals.innerHTML = data.meals
+                    .map(
+                        (meal) => `
                 <div class="meal" data-mealid="${meal.idMeal}">
                     <p>${meal.strMeal.split(' ').join('<br>')}</p>
                     <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
                 </div>
             `
-                )
-                .join('');
+                    )
+                    .join('');
+            }
         });
 }
 
 // Get meal by ID
 async function getMealById(mealId) {
-    const res = await fetch(
-        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
-    );
+    mealDetail.innerHTML = '<div class="u-meals">Wait a moment...</div>';
+    let res;
+    if (mealId) {
+        res = await fetch(
+            `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+        );
+    } else {
+        res = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    }
+
     const data = await res.json();
     const meal = data.meals[0];
-
     mealDetail.innerHTML = `
         <div class="meal-thumb" style="background-image: url(${meal.strMealThumb})">
             <div class="cuisine">
@@ -81,12 +91,18 @@ function mealIngredients(meal) {
 function cookingInstructions(meal) {
     return meal.strInstructions
         .split('.')
-        .map((step) => `<p>${step}</p>`)
-        .join('.<br>');
+        .map((step) => `<p>${step}.</p>`)
+        .join('<br>');
+}
+
+// Get randomMeal
+function getRandomMeal() {
+    getMealById();
 }
 
 // Event listeners
 form.addEventListener('submit', searchAndRenderMeals);
+randomBtn.addEventListener('click', getRandomMeal);
 meals.addEventListener('click', (event) => {
     const meal = event.path.find((el) => {
         if (el.classList) return el.classList.contains('meal');
